f3e36106c5bf813e6fe128ccae8c1c646d9ee006
pksik12 <pksik15@gmail.com>
2025-05-10 00:13:09 +0200

    Delete flowex_redesign/src directory

--- flowex_redesign/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-export function renderFooter() {
-  const footer = document.getElementById('footer');
-  footer.innerHTML = `
-    <div class="footer">
-      <p>&copy; 2025 Flowex. All rights reserved.</p>
-    </div>
-  `;
-}
-renderFooter();
